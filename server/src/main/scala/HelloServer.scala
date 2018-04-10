8fd622f3530293a5f3f7466e2cb1897f5b1a7622
Abhishek Srivastava <abhsrivastava@researchnow.com>
2018-04-10 16:06:54 -0500

    completed first implementation

--- server/src/main/scala/HelloServer.scala
+++ server/src/main/scala/HelloServer.scala
@@ -2,16 +2,27 @@ import cats.effect.Effect
 import cats.effect.IO
 import fs2._
 import fs2.StreamApp.ExitCode
+import java.nio.file.Paths
 import org.http4s.server.blaze.BlazeBuilder
 import scala.concurrent.ExecutionContext.Implicits.global
+import org.http4s.server.SSLKeyStoreSupport.StoreInfo
+import org.http4s.server.{SSLKeyStoreSupport, ServerBuilder}
+import org.http4s.server.middleware.HSTS
 
-object HelloServerMain extends HelloServer[IO]
-class HelloServer[F[_]: Effect] extends StreamApp[F] {
+object HelloServerMain extends HelloServer[IO] {
+  def builder: BlazeBuilder[IO] = BlazeBuilder[IO]
+}
+abstract class HelloServer[F[_]: Effect] extends StreamApp[F] {
+  val keypath = Paths.get("src/main/resources/server.jks").toAbsolutePath.toString
+  def builder: ServerBuilder[F] with SSLKeyStoreSupport[F]
+
+  println(s"Server Cert Path: ${keypath}")
   def stream(args: List[String], requestShutdown: F[Unit]) : fs2.Stream[F, ExitCode] = {
-    Scheduler(corePoolSize = 2).flatMap {implicit schduler => 
-      BlazeBuilder[F]
+    Scheduler(corePoolSize = 2).flatMap {implicit scheduler => 
+      builder
+        .withSSL(StoreInfo(keypath, "secret"), keyManagerPassword = "secret")
+        .mountService(HSTS(new HelloService[F].service), "/")
         .bindHttp(8080)
-        .mountService(new HelloService[F].service, "/")
         .serve
     }
   }
